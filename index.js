c7ca00c766002f724b17fa6f6372bc2f30c0f395
Nattaphong2019 <47801213+Nattaphong2019@users.noreply.github.com>
2022-07-13 00:55:03 +0700

    feat: add function randomColor

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 var canvas = document.getElementById("canvas");
-var c = canvas.getContext("2d");
-var tx = window.innerWidth;
-var ty = window.innerHeight;
-canvas.width = tx;
-canvas.height = ty;
+var context = canvas.getContext("2d");
+var totalX = window.innerWidth;
+var totalY = window.innerHeight;
+canvas.width = totalX;
+canvas.height = totalY;
+
+function randomColor() {
+  return (
+    "rgba(" +
+    Math.round(Math.random() * 250) +
+    "," +
+    Math.round(Math.random() * 250) +
+    "," +
+    Math.round(Math.random() * 250) +
+    "," +
+    Math.ceil(Math.random() * 10) / 10 +
+    ")"
+  );
+}
